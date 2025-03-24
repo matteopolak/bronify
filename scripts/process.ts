848b4fd085c63661dbd03f741047dcc37a4a4ec7
matteopolak <matteopolak@hotmail.com>
2025-03-23 20:25:03 -0400

    chore: initial commit

---
+++ scripts/process.ts
@@ -0,0 +1,212 @@
+import fs from 'node:fs';
+import crypto from 'node:crypto';
+import path from 'node:path';
+
+import axios from 'axios';
+import sharp from 'sharp';
+import Fuse from 'fuse.js';
+
+type Song = {
+	id?: string;
+	title: string;
+	artist: string;
+	thumbnail?: string;
+	tags: string[];
+
+	spotify?: string;
+	youtube: string;
+	appleMusic?: string;
+	soundcloud?: string;
+	other?: string;
+};
+
+const songs: Song[] = [
+	{
+		title: 'The LeBron That I Used To Know',
+		artist: 'ilyaugust',
+		tags: ['chill', 'lofi'],
+
+		youtube: 'Ewqz5BFC3L4'
+	},
+	{
+		title: 'Let LeBron Know',
+		artist: 'ilyaugust',
+		tags: ['chill', 'lofi'],
+
+		youtube: 'eOugkPrO-Yw'
+	},
+	{
+		title: 'LeBron Hour',
+		artist: 'ilyaugust',
+		tags: ['chill', 'lofi'],
+
+		youtube: 'NjvaYclj5f4'
+	},
+	{
+		title: 'Towards The Bron',
+		artist: 'ilyaugust',
+		tags: ['chill', 'lofi'],
+
+		youtube: '7SlHETBLpOo'
+	},
+	{
+		title: "I'd Catch A LeNade For You",
+		artist: 'ilyaugust',
+		tags: ['chill', 'lofi'],
+
+		youtube: 'oEnqveeyids'
+	},
+	{
+		title: "That's Bron",
+		artist: 'ilyaugust',
+		tags: ['chill', 'lofi'],
+
+		youtube: 'kbUbLKTzHpE'
+	},
+	{
+		title: 'I Write Brons Not Brongedies',
+		artist: 'ilyaugust',
+		tags: ['chill', 'lofi'],
+
+		youtube: '_Qgzr4RZnOE'
+	},
+	{
+		title: 'Marry Bron',
+		artist: 'ilyaugust',
+		tags: ['chill', 'lofi'],
+
+		youtube: 'V6i7vkI3dpE'
+	},
+	{
+		title: 'Bring Me Back To Bron',
+		artist: 'ilyaugust',
+		tags: ['chill', 'lofi'],
+
+		youtube: 'IY_XvFqfzds'
+	},
+	{
+		title: 'Dunk with a Smile',
+		artist: '*wicked*',
+		thumbnail: 'https://i.ytimg.com/vi/NwQPDUlJKiI/maxresdefault.jpg',
+		tags: ['chill', 'lofi'],
+
+		spotify: '03e1DoLgfvtFGeMqOQRO5g',
+		youtube: '1C7-wKtDRG0'
+	},
+	{
+		title: 'Thinking Bout Lebron',
+		artist: 'DARK MEAT UPC',
+		thumbnail: 'https://image-cdn-ak.spotifycdn.com/image/ab67706c0000da84e84b833ace730c9b3bdbb9d1',
+		tags: ['chill', 'lofi'],
+
+		spotify: '3VKiLsUzx0hT8zLAARxESG',
+		youtube: 'UUY_uOSSnYw'
+	},
+	{
+		title: 'I Believe in Lebron',
+		artist: 'DARK MEAT UPC',
+		thumbnail: 'https://i.ytimg.com/vi/t1mTgxXCc9o/maxresdefault.jpg',
+		tags: ['chill', 'lofi'],
+
+		spotify: '0lpp83OLbp8DdTzM7Q9OLi',
+		youtube: 't1mTgxXCc9o'
+	},
+	{
+		title: 'Towards The Bron',
+		artist: 'DARK MEAT UPC',
+		thumbnail: 'https://i.ytimg.com/vi/6QRPvIsH88s/maxresdefault.jpg',
+		tags: ['chill', 'lofi'],
+
+		spotify: '0izzGvHBiBNMiGPIQP76aW',
+		youtube: 'yTMTXh2pT2I'
+	},
+	{
+		title: 'Man On The Lakers',
+		artist: 'DARK MEAT UPC',
+		thumbnail:
+			'https://a57.foxnews.com/static.foxnews.com/foxnews.com/content/uploads/2023/04/896/500/lebron-hyped-playoffs.jpg?ve=1&tl=1',
+		tags: ['chill', 'lofi'],
+
+		spotify: '3doVZI24VAUTrXWDTbPTl6',
+		youtube: 'Til0sIaxzbY'
+	},
+	{
+		title: 'Thats Bron',
+		artist: 'DARK MEAT UPC',
+		thumbnail:
+			'https://www.usatoday.com/gcdn/presto/2023/04/25/USAT/ae6c67f6-bec1-4f30-aaa9-bb81e0488a51-USP_NBA__Playoffs-Memphis_Grizzlies_at_Los_Angeles_2.jpg',
+		tags: ['chill', 'lofi'],
+
+		spotify: '7Dt40sqcEpsyQ43vGyoHsP',
+		youtube: '-__U4OTAoDQ'
+	},
+	{
+		title: 'Oh Mister Lebron',
+		artist: 'DARK MEAT UPC',
+		thumbnail:
+			'https://thespun.com/.image/t_share/MjAxODY2ODQyNzgyODM2MDAy/los-angeles-lakers-v-sacramento-kings.jpg',
+		tags: ['chill', 'lofi'],
+
+		spotify: '3w327RoLVeZFqs0YttFWQp',
+		youtube: 'b1d86TJnjEo'
+	},
+	{
+		title: 'Panic At LeDisco',
+		artist: 'DARK MEAT UPC',
+		thumbnail:
+			'https://phantom-marca.unidadeditorial.es/1e46892a28a1a1e8cb49a2ed030e250d/resize/828/f/jpg/assets/multimedia/imagenes/2024/11/30/17329966091923.jpg',
+		tags: ['chill', 'lofi'],
+
+		spotify: '2DXWNVR00eZMyVEQp5qx2y',
+		youtube: 's9g_Qo-YgvY'
+	}
+] satisfies Song[];
+
+const outDir = 'src/lib/content';
+
+// delete directory
+try {
+	fs.rmSync(outDir, { recursive: true });
+} catch {}
+// create directory
+fs.mkdirSync(outDir, { recursive: true });
+
+async function main() {
+	for (const song of songs) {
+		// generate hash from title and artist
+		const hash = crypto.createHash('sha256');
+
+		hash.update(song.title);
+		hash.update(song.artist);
+
+		const id = hash.digest('hex');
+
+		song.id = id;
+
+		// parse thumbnail
+		const stream = await axios.get(
+			song.thumbnail ?? `https://i.ytimg.com/vi/${song.youtube}/maxresdefault.jpg`,
+			{ responseType: 'arraybuffer' }
+		);
+		const buffer = Buffer.from(stream.data);
+
+		// process into 512x512 thumbnail (webp)
+		const result = await sharp(buffer).resize(512, 512, { fit: 'cover' }).webp().toBuffer();
+
+		fs.writeFileSync(path.join(outDir, `${id}.webp`), result);
+
+		// @ts-expect-error - a
+		song.thumbnail = undefined;
+	}
+
+	songs.sort((a, b) => a.id!.localeCompare(b.id!));
+
+	// write songs
+	fs.writeFileSync(path.join(outDir, 'songs.json'), JSON.stringify(songs));
+
+	const index = Fuse.createIndex(['title', 'artist', 'tags'], songs);
+
+	fs.writeFileSync(path.join(outDir, 'index.json'), JSON.stringify(index.toJSON()));
+}
+
+main();
---
+++ src/routes/+page.svelte
@@ -0,0 +1,279 @@
+<script lang="ts">
+	import bronify from '$lib/images/bronify.png?enhanced';
+	import songData from '$lib/content/songs.json';
+
+	import { Play, Pause } from '@lucide/svelte';
+	import Player from 'youtube-player';
+	import type { YouTubePlayer } from 'youtube-player/dist/types';
+
+	type Song = (typeof songData)[0];
+
+	const thumbnails: Record<string, { default: string }> = import.meta.glob(
+		'/src/lib/content/*.webp',
+		{
+			query: '?url',
+			eager: true
+		}
+	);
+
+	import { shortcut, type ShortcutEventDetail } from '@svelte-put/shortcut';
+	import Fuse from 'fuse.js';
+	import { onMount } from 'svelte';
+
+	const songIndex = new Fuse(songData, {
+		keys: ['title', 'artist', 'tags'],
+		threshold: 0.4
+	});
+
+	let searchInput: HTMLInputElement = $state()!;
+	let comingSoonModal: HTMLDialogElement = $state()!;
+
+	function handleK(detail: ShortcutEventDetail) {
+		detail.originalEvent.preventDefault();
+		searchInput.focus();
+	}
+
+	let search = $state('');
+	let songs = $derived(
+		search ? songIndex.search(search, { limit: 15 }).map((s) => s.item) : songData
+	);
+
+	let playing: Song | undefined = $state();
+	let status = $state({
+		paused: true,
+		lengthSeconds: 60,
+		currentSeconds: 0
+	});
+
+	async function toggle(song: Song) {
+		if (playing?.id !== song.id) {
+			playing = undefined;
+
+			await player.loadVideoById(song.youtube);
+			await player.playVideo();
+
+			status.currentSeconds = 0;
+			status.paused = false;
+
+			playing = song;
+
+			return;
+		}
+
+		status.paused = !status.paused;
+
+		if (status.paused) {
+			await player.pauseVideo();
+		} else {
+			await player.playVideo();
+		}
+	}
+
+	let playerElement: HTMLDivElement = $state()!;
+	let player: YouTubePlayer = $state()!;
+
+	onMount(() => {
+		player = Player(playerElement, {
+			playerVars: {
+				autoplay: 0
+			}
+		});
+		player.setVolume(1);
+
+		player.on('stateChange', async (event) => {
+			if (event.data === 1) {
+				status.lengthSeconds = await player.getDuration();
+			}
+
+			if (event.data === 0) {
+				status.paused = true;
+			}
+		});
+
+		setInterval(async () => {
+			if (status.paused) return;
+
+			const seconds = (await player.getCurrentTime()) ?? 0;
+			status.currentSeconds = seconds;
+		}, 100);
+	});
+
+	function onMouseDown(event: MouseEvent) {
+		const progress = event.target as HTMLProgressElement;
+		const value = event.offsetX / progress.offsetWidth;
+
+		status.currentSeconds = status.lengthSeconds * value;
+		player.seekTo(status.currentSeconds, true);
+	}
+
+	function formatSeconds(seconds: number) {
+		const minutes = Math.floor(seconds / 60);
+		const remainingSeconds = Math.floor(seconds % 60);
+
+		return `${minutes}:${remainingSeconds.toString().padStart(2, '0')}`;
+	}
+</script>
+
+<svelte:head>
+	<link rel="icon" href={bronify.img.src} />
+	<title>Bronify: LeMusic for everyone</title>
+</svelte:head>
+
+<svelte:window
+	use:shortcut={{
+		trigger: {
+			key: 'k',
+			modifier: ['ctrl', 'meta'],
+			callback: handleK
+		}
+	}}
+/>
+
+<div bind:this={playerElement} class="hidden"></div>
+
+<dialog bind:this={comingSoonModal} id="coming-soon-modal" class="modal">
+	<div class="modal-box bg-base-200 rounded-2xl">
+		<form method="dialog">
+			<button class="btn btn-sm btn-circle btn-ghost absolute top-2 right-2">✕</button>
+		</form>
+
+		<h3 class="text-lg font-bold">Coming soon</h3>
+		<p class="py-4">This feature is coming soon, stay tuned!</p>
+	</div>
+
+	<form method="dialog" class="modal-backdrop">
+		<button>close</button>
+	</form>
+</dialog>
+
+<div class="navbar px-12">
+	<div class="navbar-start">
+		<a href="/" class="flex flex-row place-items-center gap-2" aria-label="Bronify Home">
+			<enhanced:img src={bronify} alt="Bronify" sizes="48px" class="h-auto w-auto" />
+		</a>
+	</div>
+
+	<label class="input input-lg navbar-center w-sm">
+		<svg class="h-[1.5em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
+			><g
+				stroke-linejoin="round"
+				stroke-linecap="round"
+				stroke-width="2.5"
+				fill="none"
+				stroke="currentColor"
+				><circle cx="11" cy="11" r="8"></circle><path d="m21 21-4.3-4.3"></path></g
+			></svg
+		>
+		<input
+			bind:this={searchInput}
+			bind:value={search}
+			type="search"
+			class="grow"
+			placeholder="What do you want to play?"
+		/>
+		<kbd class="kbd kbd-sm">⌘</kbd>
+		<kbd class="kbd kbd-sm">K</kbd>
+	</label>
+
+	<div class="navbar-end gap-2">
+		<button class="btn btn-md" onclick={() => comingSoonModal.showModal()}>Sign in</button>
+		<button class="btn btn-md btn-primary" onclick={() => comingSoonModal.showModal()}
+			>Sign up</button
+		>
+	</div>
+</div>
+
+<!-- Search results -->
+{#snippet cover(song: Song)}
+	{@const url = thumbnails[`/src/lib/content/${song.id}.webp`].default}
+
+	<div
+		class="group card hover:bg-base-200 flex w-auto flex-col gap-2 p-2 transition-all duration-100"
+	>
+		<div class="relative">
+			<img src={url} alt={song.title} sizes="100px" class="h-auto w-auto rounded-lg" />
+
+			<!-- Play button -->
+			<button
+				onclick={() => toggle(song)}
+				class="bg-primary absolute right-2 bottom-2 cursor-pointer rounded-full p-3.5 text-left text-black opacity-0 transition-all duration-100 ease-in-out group-hover:opacity-100 hover:right-1.5 hover:bottom-1.5 hover:p-4"
+			>
+				{#if song.id === playing?.id && !status.paused}
+					<Pause fill="currentColor" size="1.5em" />
+				{:else}
+					<Play fill="currentColor" size="1.5em" />
+				{/if}
+			</button>
+		</div>
+
+		<div class="flex flex-col gap-2">
+			<h3 class="text-md font-semibold">{song.title}</h3>
+			<p class="text-sm text-slate-300">By {song.artist}</p>
+		</div>
+
+		<div class="flex flex-row gap-1">
+			{#each song.tags as tag (tag)}
+				<span class="badge badge-ghost">{tag}</span>
+			{/each}
+		</div>
+	</div>
+{/snippet}
+
+<div class="flex w-full place-content-center">
+	<div
+		class="grid max-w-7xl grid-cols-1 gap-8 p-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5"
+	>
+		{#each songs as song (song.id)}
+			{@render cover(song)}
+		{/each}
+	</div>
+</div>
+
+<!-- Sticky to bottom of page, current song status -->
+{#snippet controls(song: Song)}
+	{@const url = thumbnails[`/src/lib/content/${song.id}.webp`].default}
+
+	<div class="navbar-start h-16 grow gap-2 self-start md:self-auto">
+		<img src={url} alt={song.title} sizes="16px" class="h-full rounded-lg" />
+
+		<div>
+			<h3 class="text-sm font-semibold">{song.title}</h3>
+			<p class="text-xs text-slate-300">By {song.artist}</p>
+		</div>
+	</div>
+
+	<div class="navbar-end lg:navbar-center w-auto max-w-lg flex-col gap-2 lg:w-full">
+		<button
+			onclick={() => toggle(song)}
+			class="bg-base-content cursor-pointer rounded-full p-2 text-left text-black transition-all duration-100 ease-in-out hover:-m-0.5 hover:p-2.5"
+		>
+			{#if !status.paused}
+				<Pause fill="currentColor" size="1em" />
+			{:else}
+				<Play fill="currentColor" size="1em" />
+			{/if}
+		</button>
+
+		<div class="flex flex-row place-items-center gap-2 lg:w-full">
+			<span class="text-xs text-slate-300">
+				{formatSeconds(status.currentSeconds)}
+			</span>
+			<button onmousedown={onMouseDown} class="hidden w-full lg:flex">
+				<progress
+					class="progress h-1.5 w-full"
+					value={status.currentSeconds / status.lengthSeconds}
+					max={1}
+				></progress>
+			</button>
+			<span class="lg:hidden"> / </span>
+			<span class="text-xs text-slate-300">
+				{formatSeconds(status.lengthSeconds)}
+			</span>
+		</div>
+	</div>
+	<div class="lg:navbar-end"></div>
+{/snippet}
+
+<div class="bg-base-100 navbar fixed right-0 bottom-0 left-0 h-20 flex-row gap-0 p-4">
+	{@render controls(playing ?? songData[0])}
+</div>
