3fba5bad736bfa3f4e9a417e1a62bf4804de42f0
matteopolak <matteopolak@hotmail.com>
2025-03-27 23:54:09 -0400

    feat: update tags, sort artists by song count

--- scripts/pre_build.ts
+++ scripts/pre_build.ts
@@ -4,16 +4,28 @@ const tracks = JSON.parse(fs.readFileSync('src/lib/content/tracks.json', 'utf-8'
 
 const artists = JSON.parse(fs.readFileSync('src/lib/content/artists.json', 'utf-8'));
 
+const tracksByArtist = new Map<string, string[]>();
+
+for (const track of tracks) {
+	if (!tracksByArtist.has(track.artist)) {
+		tracksByArtist.set(track.artist, []);
+	}
+	tracksByArtist.get(track.artist)?.push(track.id);
+}
+
 // sort tracks by id
 tracks.sort((a, b) => {
 	return a.id.localeCompare(b.id);
 });
 
-// sort artists by id
+// sort artists by number of tracks. if equal, sort by id
 artists.sort((a, b) => {
+	if (tracksByArtist.get(b.id)?.length !== tracksByArtist.get(a.id)?.length) {
+		return (tracksByArtist.get(b.id)?.length || 0) - (tracksByArtist.get(a.id)?.length || 0);
+	}
 	return a.id.localeCompare(b.id);
 });
 
-fs.writeFileSync('src/lib/content/tracks.json', JSON.stringify(tracks, null, 2), 'utf-8');
+fs.writeFileSync('src/lib/content/tracks.json', JSON.stringify(tracks, null, '\t'), 'utf-8');
 
-fs.writeFileSync('src/lib/content/artists.json', JSON.stringify(artists, null, 2), 'utf-8');
+fs.writeFileSync('src/lib/content/artists.json', JSON.stringify(artists, null, '\t'), 'utf-8');
