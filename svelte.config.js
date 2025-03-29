0b4ac9114df58aaf91245e284f81caa91ec466be
matteopolak <matteopolak@hotmail.com>
2025-03-28 22:46:06 -0400

    feat: add cloudflare toggle

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,17 @@
 import adapter from '@sveltejs/adapter-static';
+import cloudflare from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const useCloudflare = process.env.CF_PAGES === '1';
 
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter({
-			fallback: 'index.html'
-		})
+		adapter: useCloudflare
+			? cloudflare()
+			: adapter({
+					fallback: 'index.html'
+				})
 	}
 };
 
