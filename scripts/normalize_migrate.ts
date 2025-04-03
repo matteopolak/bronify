1564c1cb52ef1c8f90e002feb3548e863003693d
matteopolak <matteopolak@hotmail.com>
2025-04-03 00:39:43 -0400

    feat: add new script

---
+++ scripts/normalize_migrate.ts
@@ -0,0 +1,77 @@
+import fs from 'node:fs';
+import { execFileSync } from 'node:child_process';
+import crypto from 'node:crypto';
+
+const tracks = JSON.parse(fs.readFileSync('src/lib/content/tracks.json', 'utf-8'));
+
+const oldToNewId: Record<string, string> = {};
+
+for (const track of tracks) {
+	// old id
+	const id = track.id;
+
+	console.log(
+		[
+			'run',
+			'ffmpeg-normalize',
+			`../src/lib/content/tracks/${id}/audio.mp3`,
+			'-o',
+			`../src/lib/content/tracks/${id}/audio-normalized.mp3`,
+			'-c:a',
+			'libmp3lame',
+			'-b:a',
+			'192k'
+		].join(' ')
+	);
+
+	// normalize audio
+	execFileSync(
+		'uv',
+		[
+			'run',
+			'ffmpeg-normalize',
+			`../src/lib/content/tracks/${id}/audio.mp3`,
+			'-o',
+			`../src/lib/content/tracks/${id}/audio-normalized.mp3`,
+			'-c:a',
+			'aac',
+			'-b:a',
+			'192k'
+		],
+		{
+			cwd: 'scripts'
+		}
+	);
+
+	// compute new id
+	const hash = crypto.createHash('sha256');
+	const file = fs.readFileSync(`src/lib/content/tracks/${id}/audio-normalized.mp3`);
+
+	hash.update(file);
+
+	const newId = hash.digest('base64url').slice(0, 8);
+
+	// rename audio
+	fs.unlinkSync(`src/lib/content/tracks/${id}/audio.mp3`);
+	fs.renameSync(
+		`src/lib/content/tracks/${id}/audio-normalized.mp3`,
+		`src/lib/content/tracks/${id}/audio.mp3`
+	);
+
+	// move folder
+	fs.renameSync(`src/lib/content/tracks/${id}`, `src/lib/content/tracks/${newId}`);
+
+	// update id
+	track.id = newId;
+}
+
+const albums = JSON.parse(fs.readFileSync('src/lib/content/albums.json', 'utf-8'));
+
+for (const album of albums) {
+	album.tracks = album.tracks.map((track: string) => {
+		return oldToNewId[track] ?? track;
+	});
+}
+
+fs.writeFileSync('src/lib/content/tracks.json', JSON.stringify(tracks, null, '\t'));
+fs.writeFileSync('src/lib/content/albums.json', JSON.stringify(albums, null, '\t'));
